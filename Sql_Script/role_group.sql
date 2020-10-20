d12bc486022e4ebb7fbdfcaa96eb844fb8576181
hawile <hawile@163.com>
2020-10-21 00:24:23 +0800

    keydom

--- Sql_Script/role_group.sql
+++ Sql_Script/role_group.sql
@@ -5,32 +5,32 @@
  Source Server Type    : MySQL
  Source Server Version : 50729
  Source Host           : localhost:3306
- Source Schema         : hawile
+ Source Schema         : keydom
 
  Target Server Type    : MySQL
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 20/10/2020 21:34:08
+ Date: 21/10/2020 00:22:31
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for auth_group
+-- Table structure for role_group
 -- ----------------------------
-DROP TABLE IF EXISTS `auth_group`;
-CREATE TABLE `auth_group` (
+DROP TABLE IF EXISTS `role_group`;
+CREATE TABLE `role_group` (
   `group_id` int(11) NOT NULL AUTO_INCREMENT,
   `group_name` varchar(255) DEFAULT NULL,
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`group_id`),
-  CONSTRAINT `fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_value` (`group_id`)
+  CONSTRAINT `fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `role_to_group` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of auth_group
+-- Records of role_group
 -- ----------------------------
 BEGIN;
 COMMIT;
--- Sql_Script/role_to_group.sql
+++ Sql_Script/role_to_group.sql
@@ -5,23 +5,23 @@
  Source Server Type    : MySQL
  Source Server Version : 50729
  Source Host           : localhost:3306
- Source Schema         : hawile
+ Source Schema         : keydom
 
  Target Server Type    : MySQL
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 20/10/2020 21:34:14
+ Date: 21/10/2020 00:22:38
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for auth_value
+-- Table structure for role_to_group
 -- ----------------------------
-DROP TABLE IF EXISTS `auth_value`;
-CREATE TABLE `auth_value` (
+DROP TABLE IF EXISTS `role_to_group`;
+CREATE TABLE `role_to_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `group_id` int(11) NOT NULL,
   `auth_id` int(11) NOT NULL DEFAULT '0',
@@ -30,11 +30,11 @@ CREATE TABLE `auth_value` (
   PRIMARY KEY (`id`),
   KEY `fk_auth_id` (`auth_id`),
   KEY `group_id` (`group_id`),
-  CONSTRAINT `fk_auth_id` FOREIGN KEY (`auth_id`) REFERENCES `auth` (`auth_id`)
+  CONSTRAINT `fk_auth_id` FOREIGN KEY (`auth_id`) REFERENCES `role` (`auth_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of auth_value
+-- Records of role_to_group
 -- ----------------------------
 BEGIN;
 COMMIT;
