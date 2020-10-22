360a5a8305b30be0cad2142f948571b1220390de
hawile <hawile@163.com>
2020-10-22 19:34:05 +0800

    keydom

--- Sql_Script/role.sql
+++ Sql_Script/role.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 21/10/2020 16:57:07
+ Date: 22/10/2020 19:21:42
 */
 
 SET NAMES utf8mb4;
@@ -22,96 +22,98 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `symbol` varchar(255) DEFAULT NULL,
-  `parent_id` int(11) DEFAULT '0',
-  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `sort` int(11) DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) DEFAULT NULL COMMENT '权限名称',
+  `symbol` varchar(255) DEFAULT NULL COMMENT '权限标识',
+  `parent_id` int(11) DEFAULT '0' COMMENT '父权限ID',
+  `sort` int(11) DEFAULT '0' COMMENT '排序',
+  `icon` varchar(255) DEFAULT NULL COMMENT '图标',
+  `href` varchar(255) DEFAULT NULL COMMENT '链接',
+  `menu_id` int(1) DEFAULT '0' COMMENT '菜单类型',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
 BEGIN;
-INSERT INTO `role` VALUES (1, '信息管理', 'information', 0, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (2, '厂商信息', 'contact', 1, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (3, '账号信息', 'account', 1, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (4, '客户账号信息', 'csr', 1, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (5, '售货机订单信息', 'atm', 1, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (6, '工作报表', 'work', 1, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (7, '文件管理', 'files', 0, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (8, '表单信息', 'form', 7, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (9, '文件信息', 'file', 7, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (10, '软件管理', 'soft', 0, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (11, '应用软件', 'softApp', 10, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (12, '驱动软件', 'softDrive', 10, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (13, '系统软件', 'softOs', 10, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (14, '系统管理', 'system', 0, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (15, '系统日志', 'log', 14, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (16, '用户管理', 'users', 0, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (17, '权限管理', 'role', 16, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (18, '职位管理', 'post', 16, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (19, '用户管理', 'user', 16, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (20, '用户申请管理', 'userSign', 16, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (21, '个人信息', 'userInfo', 16, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (22, '添加信息', 'contact-add', 2, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (23, '编辑信息', 'contact-edit', 2, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (24, '删除信息', 'contact-delete', 2, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (25, '添加账号', 'account-add', 3, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (26, '编辑账号', 'account-edit', 3, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (27, '删除账号', 'account-delete', 3, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (28, '添加账号', 'csr-add', 4, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (29, '编辑账号', 'csr-edit', 4, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (30, '删除账号', 'csr-delete', 4, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (31, '打印列表', 'csr-print', 4, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (32, '添加订单', 'atm-add', 5, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (33, '编辑订单', 'atm-edit', 5, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (34, '删除订单', 'atm-delete', 5, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (35, '添加商品', 'goods-add', 5, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (36, '编辑商品', 'goods-edit', 5, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (37, '删除商品', 'goods-delete', 5, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (38, '添加内容', 'work-add', 6, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (39, '编辑内容', 'work-edit', 6, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (40, '删除内容', 'work-delete', 6, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (41, '打印列表', 'work-print', 6, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (42, '添加表单', 'form-add', 8, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (43, '编辑表单', 'form-edit', 8, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (44, '删除表单', 'form-delete', 8, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (45, '填写表单', 'form-input', 8, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (46, '下载表单', 'form-download', 8, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (47, '添加文件', 'file-add', 9, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (48, '编辑文件', 'file-edit', 9, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (49, '删除文件', 'file-delete', 9, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (50, '下载文件', 'file-download', 9, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (51, '添加软件', 'softApp-add', 11, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (52, '编辑软件', 'softApp-edit', 11, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (53, '删除软件', 'softApp-delete', 11, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (54, '下载软件', 'softApp-download', 11, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (55, '添加软件', 'softDrive-add', 12, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (56, '编辑软件', 'softDrive-edit', 12, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (57, '删除软件', 'softDrive-delete', 12, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (58, '下载软件', 'softDrive-download', 12, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (59, '添加软件', 'softOs-add', 13, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (60, '编辑软件', 'softOs-edit', 13, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (61, '删除软件', 'softOs-delete', 13, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (62, '浏览地址', 'softOs-view', 13, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (63, '添加权限', 'role-add', 17, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (64, '编辑权限', 'role-edit', 17, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (65, '删除权限', 'role-delete', 17, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (66, '添加职位', 'job-add', 18, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (67, '编辑职位', 'job-edit', 18, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (68, '删除职位', 'job-delete', 18, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (69, '添加部门', 'dept-add', 18, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (70, '编辑部门', 'dept-edit', 18, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (71, '删除部门', 'dept-delete', 18, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (72, '添加用户', 'user-add', 19, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (73, '编辑用户', 'user-edit', 19, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (74, '删除用户', 'user-delete', 19, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (75, '操作申请', 'userSign-edit', 20, '2020-10-21 16:00:14', 0);
-INSERT INTO `role` VALUES (76, '编辑信息', 'userInfo-edit', 21, '2020-10-21 16:00:14', 0);
+INSERT INTO `role` VALUES (1, '信息管理', 'information', 0, 0, 'fa fa-envelope', NULL, 0);
+INSERT INTO `role` VALUES (2, '厂商信息', 'contact', 1, 0, 'fa fa-envelope-square', 'contact', 1);
+INSERT INTO `role` VALUES (3, '账号信息', 'account', 1, 0, 'fa fa-envelope-square', 'acc_num', 1);
+INSERT INTO `role` VALUES (4, '客户账号信息', 'csr', 1, 0, 'fa fa-envelope-square', 'csr', 1);
+INSERT INTO `role` VALUES (5, '售货机订单信息', 'atm', 1, 0, 'fa fa-bar-chart', 'atm', 1);
+INSERT INTO `role` VALUES (6, '工作报表', 'work', 1, 0, 'fa fa-file-word-o', 'work', 1);
+INSERT INTO `role` VALUES (7, '文件管理', 'files', 0, 0, 'fa fa-folder', NULL, 0);
+INSERT INTO `role` VALUES (8, '表单信息', 'form', 7, 0, 'fa fa-folder', 'form', 1);
+INSERT INTO `role` VALUES (9, '文件信息', 'file', 7, 0, 'fa fa-folder', 'file', 1);
+INSERT INTO `role` VALUES (10, '软件管理', 'soft', 0, 0, 'fa fa-adn', NULL, 0);
+INSERT INTO `role` VALUES (11, '应用软件', 'softApp', 10, 0, 'fa fa-adn', 'soft_app', 1);
+INSERT INTO `role` VALUES (12, '驱动软件', 'softDrive', 10, 0, 'fa fa-adn', 'soft_drive', 1);
+INSERT INTO `role` VALUES (13, '系统软件', 'softOs', 10, 0, 'fa fa-adn', 'soft_os', 1);
+INSERT INTO `role` VALUES (14, '系统管理', 'system', 0, 0, 'fa fa-gear', NULL, 0);
+INSERT INTO `role` VALUES (15, '系统日志', 'log', 14, 0, 'fa fa-file-text', 'log', 1);
+INSERT INTO `role` VALUES (16, '用户管理', 'users', 0, 0, 'fa fa-users', NULL, 0);
+INSERT INTO `role` VALUES (17, '权限管理', 'role', 16, 0, 'fa fa-sun-o', 'role', 1);
+INSERT INTO `role` VALUES (18, '职位管理', 'post', 16, 0, 'fa fa-user', 'job', 1);
+INSERT INTO `role` VALUES (19, '用户管理', 'user', 16, 0, 'fa fa-users', 'user', 1);
+INSERT INTO `role` VALUES (20, '用户申请管理', 'userSign', 16, 0, 'fa fa-user-plus', 'user_sign', 1);
+INSERT INTO `role` VALUES (21, '个人信息', 'userInfo', 16, 0, 'fa fa-user', 'user_info', 1);
+INSERT INTO `role` VALUES (22, '添加信息', 'contact-add', 2, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (23, '编辑信息', 'contact-edit', 2, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (24, '删除信息', 'contact-delete', 2, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (25, '添加账号', 'account-add', 3, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (26, '编辑账号', 'account-edit', 3, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (27, '删除账号', 'account-delete', 3, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (28, '添加账号', 'csr-add', 4, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (29, '编辑账号', 'csr-edit', 4, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (30, '删除账号', 'csr-delete', 4, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (31, '打印列表', 'csr-print', 4, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (32, '添加订单', 'atm-add', 5, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (33, '编辑订单', 'atm-edit', 5, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (34, '删除订单', 'atm-delete', 5, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (35, '添加商品', 'goods-add', 5, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (36, '编辑商品', 'goods-edit', 5, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (37, '删除商品', 'goods-delete', 5, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (38, '添加内容', 'work-add', 6, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (39, '编辑内容', 'work-edit', 6, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (40, '删除内容', 'work-delete', 6, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (41, '打印列表', 'work-print', 6, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (42, '添加表单', 'form-add', 8, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (43, '编辑表单', 'form-edit', 8, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (44, '删除表单', 'form-delete', 8, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (45, '填写表单', 'form-input', 8, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (46, '下载表单', 'form-download', 8, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (47, '添加文件', 'file-add', 9, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (48, '编辑文件', 'file-edit', 9, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (49, '删除文件', 'file-delete', 9, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (50, '下载文件', 'file-download', 9, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (51, '添加软件', 'softApp-add', 11, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (52, '编辑软件', 'softApp-edit', 11, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (53, '删除软件', 'softApp-delete', 11, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (54, '下载软件', 'softApp-download', 11, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (55, '添加软件', 'softDrive-add', 12, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (56, '编辑软件', 'softDrive-edit', 12, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (57, '删除软件', 'softDrive-delete', 12, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (58, '下载软件', 'softDrive-download', 12, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (59, '添加软件', 'softOs-add', 13, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (60, '编辑软件', 'softOs-edit', 13, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (61, '删除软件', 'softOs-delete', 13, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (62, '浏览地址', 'softOs-view', 13, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (63, '添加权限', 'role-add', 17, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (64, '编辑权限', 'role-edit', 17, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (65, '删除权限', 'role-delete', 17, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (66, '添加职位', 'job-add', 18, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (67, '编辑职位', 'job-edit', 18, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (68, '删除职位', 'job-delete', 18, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (69, '添加部门', 'dept-add', 18, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (70, '编辑部门', 'dept-edit', 18, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (71, '删除部门', 'dept-delete', 18, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (72, '添加用户', 'user-add', 19, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (73, '编辑用户', 'user-edit', 19, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (74, '删除用户', 'user-delete', 19, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (75, '操作申请', 'userSign-edit', 20, 0, NULL, NULL, 2);
+INSERT INTO `role` VALUES (76, '编辑信息', 'userInfo-edit', 21, 0, NULL, NULL, 2);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- Sql_Script/role_to_user.sql
+++ Sql_Script/role_to_user.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 21/10/2020 16:56:57
+ Date: 22/10/2020 19:21:49
 */
 
 SET NAMES utf8mb4;
@@ -24,14 +24,172 @@ DROP TABLE IF EXISTS `role_to_user`;
 CREATE TABLE `role_to_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `auth_id` int(11) NOT NULL DEFAULT '0',
+  `role_id` int(11) NOT NULL DEFAULT '0',
   `value` int(1) DEFAULT '0',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `fk_auth_id` (`auth_id`),
+  KEY `fk_auth_id` (`role_id`),
   KEY `group_id` (`user_id`),
-  CONSTRAINT `fk_auth_id` FOREIGN KEY (`auth_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `fk_auth_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=153 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_to_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `role_to_user` VALUES (1, 1, 1, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (2, 1, 7, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (3, 1, 10, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (4, 1, 14, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (5, 1, 16, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (6, 1, 2, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (7, 1, 3, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (8, 1, 4, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (9, 1, 5, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (10, 1, 6, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (11, 1, 22, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (12, 1, 23, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (13, 1, 24, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (14, 1, 25, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (15, 1, 26, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (16, 1, 27, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (17, 1, 28, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (18, 1, 29, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (19, 1, 30, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (20, 1, 31, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (21, 1, 32, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (22, 1, 33, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (23, 1, 34, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (24, 1, 35, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (25, 1, 36, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (26, 1, 37, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (27, 1, 38, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (28, 1, 39, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (29, 1, 40, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (30, 1, 41, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (31, 1, 8, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (32, 1, 9, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (33, 1, 42, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (34, 1, 43, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (35, 1, 44, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (36, 1, 45, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (37, 1, 46, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (38, 1, 47, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (39, 1, 48, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (40, 1, 49, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (41, 1, 50, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (42, 1, 11, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (43, 1, 12, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (44, 1, 13, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (45, 1, 51, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (46, 1, 52, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (47, 1, 53, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (48, 1, 54, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (49, 1, 55, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (50, 1, 56, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (51, 1, 57, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (52, 1, 58, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (53, 1, 59, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (54, 1, 60, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (55, 1, 61, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (56, 1, 62, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (57, 1, 15, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (58, 1, 17, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (59, 1, 18, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (60, 1, 19, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (61, 1, 20, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (62, 1, 21, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (63, 1, 63, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (64, 1, 64, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (65, 1, 65, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (66, 1, 66, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (67, 1, 67, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (68, 1, 68, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (69, 1, 69, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (70, 1, 70, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (71, 1, 71, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (72, 1, 72, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (73, 1, 73, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (74, 1, 74, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (75, 1, 75, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (76, 1, 76, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (77, 3, 1, 1, '2020-10-22 09:46:45');
+INSERT INTO `role_to_user` VALUES (78, 3, 7, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (79, 3, 10, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (80, 3, 14, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (81, 3, 16, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (82, 3, 2, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (83, 3, 3, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (84, 3, 4, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (85, 3, 5, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (86, 3, 6, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (87, 3, 22, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (88, 3, 23, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (89, 3, 24, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (90, 3, 25, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (91, 3, 26, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (92, 3, 27, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (93, 3, 28, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (94, 3, 29, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (95, 3, 30, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (96, 3, 31, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (97, 3, 32, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (98, 3, 33, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (99, 3, 34, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (100, 3, 35, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (101, 3, 36, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (102, 3, 37, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (103, 3, 38, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (104, 3, 39, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (105, 3, 40, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (106, 3, 41, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (107, 3, 8, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (108, 3, 9, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (109, 3, 42, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (110, 3, 43, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (111, 3, 44, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (112, 3, 45, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (113, 3, 46, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (114, 3, 47, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (115, 3, 48, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (116, 3, 49, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (117, 3, 50, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (118, 3, 11, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (119, 3, 12, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (120, 3, 13, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (121, 3, 51, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (122, 3, 52, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (123, 3, 53, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (124, 3, 54, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (125, 3, 55, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (126, 3, 56, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (127, 3, 57, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (128, 3, 58, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (129, 3, 59, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (130, 3, 60, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (131, 3, 61, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (132, 3, 62, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (133, 3, 15, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (134, 3, 17, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (135, 3, 18, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (136, 3, 19, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (137, 3, 20, 1, '2020-10-22 09:53:58');
+INSERT INTO `role_to_user` VALUES (138, 3, 21, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (139, 3, 63, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (140, 3, 64, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (141, 3, 65, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (142, 3, 66, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (143, 3, 67, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (144, 3, 68, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (145, 3, 69, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (146, 3, 70, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (147, 3, 71, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (148, 3, 72, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (149, 3, 73, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (150, 3, 74, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (151, 3, 75, 1, '2020-10-22 09:29:50');
+INSERT INTO `role_to_user` VALUES (152, 3, 76, 1, '2020-10-22 09:29:50');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
